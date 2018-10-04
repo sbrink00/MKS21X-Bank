b5a7134ce822805eb91ec15f6001eddbea5c20d4
sbrink00 <sbrink00@stuy.edu>
2018-10-03 23:54:43 -0400

    printed out balance and account ID

--- BankAccount.java
+++ BankAccount.java
@@ -1,20 +1,58 @@
 public class BankAccount{
+  private double balance;
+  private int accountID;
+  private String password;
 
+/*Sole constructor requires all field's values*/
+   public BankAccount( int newaccountID, double newbalance, String newpassword){
+     balance = newbalance;
+     accountID = newaccountID;
+     password = newpassword;
+   }
+  /*Return a String to be used to display the account data. "ACCOUNT\tBALANCE" */
+   public String toString(){
+     return "Balance: " + balance + "\nAccount ID: " + accountID;
+   }
+  /*standard accessor methods*/
+   public double getBalance(){
+     return balance;
+   }
+   public int getAccountID(){
+     return accountID;
+   }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  /*Change the value of password to the specified value
+   *@param newPass The value to replace the old password with*/
+    public void setPassword(String newPass){
+      password = newPass;
+    }
+  /*Only when amount is positive: Increase balance by amount and return true.
+   *@return When amount is positive: Return true, otherwise return false.
+   *@param amount The amount is how much to change the balance by.
+   */
+    public boolean deposit(double amount){
+      if (amount >= 0){
+        balance += amount;
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+  /*Only when amount is positive and the amount does not exceed the balance:
+   *Decrease balance by amount and return true, return false otherwise.
+   *Make no change if the amount is too large to withdraw
+   *@param amount The amount is how much to change the balance by.
+   *@return true when the balance is large enough, false otherwise.
+   */
+    public boolean withdraw(double amount){
+      if (amount >= 0 && amount <= balance){
+        balance -= amount;
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
 
 }
--- Driver.java
+++ Driver.java
@@ -1,8 +1,7 @@
 public class Driver{
   public static void main(String[] args){
-      
-
-
+    BankAccount output = new BankAccount(10101, 1000.00, "qwerty");
+    System.out.println(output);
 
 
 
